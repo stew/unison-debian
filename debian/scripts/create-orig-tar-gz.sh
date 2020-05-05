453f00c847bbdc7dd29c4a31550ce67ac5174410
Stew O'Connor <stew@vireo.org>
2020-05-05 14:52:07 -0700

    import packaging scripts

--- debian/scripts/create-orig-tar-gz.sh
+++ debian/scripts/create-orig-tar-gz.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-pre_flight() {
-    which jq > /dev/null || (echo "this script requires jq to be installed" && exit)
-}
-
 usage() {
     echo "Usage: $0 version tag \n\
 \n\
@@ -14,17 +10,17 @@ Example: $0 0.0.1~M1j release/M1j\n\
 exit 1
 }
 
-pre_flight
-
 if [ $# -ne 2 ]; then
     usage
 fi
 
-uriencode() { jq -nr --arg v "$1" '$v|@uri'; }
-
 version=$1 ; shift
-release=$(uriencode $1) ; shift
+release=$1 ; shift
 
-release_url=https://github.com/unisonweb/unison/archive/$release.tar.gz
+tmpdir=$(mktemp -d)
 
-wget -O../unisonweb-$version.orig.tar.gz $release_url
+git clone --recursive -b ${release} https://github.com/unisonweb/unison $tmpdir/unison
+
+tar -C ${tmpdir} --exclude .git -czf unison_${version}.orig.tar.gz unison
+
+rm -rf $tmpdir
